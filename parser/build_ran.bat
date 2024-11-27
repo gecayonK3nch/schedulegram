d466668ddc84d8a562c57b9dfd8520196b4a9458
K3nch <gecayon.k3nch@yandex.ru>
2024-11-27 12:54:46 +0300

    replaced .gitignores with one 4 all. Parser v.1.0 added

---
+++ parser/build_ran.bat
@@ -0,0 +1,15 @@
+@echo off
+chcp 65001
+
+set BUILD_DIR = build
+if exist %BUILD_DIR% rmdir /s /q %BUILD_DIR%
+
+cmake -B build -S . -G "Visual Studio 17 2022"
+cmake --build build
+cd build\Debug
+
+parser.exe
+
+pause
+cd ..
+cd ..
